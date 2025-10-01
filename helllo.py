0957952a4c729723170b2760284b90bf417974cd
MR-just01 <rawatmaya2908@gmail.com>
2025-10-01 23:25:11 +0530

    initial commit

---
+++ helllo.py
@@ -0,0 +1,60 @@
+# print("hello")
+# a=4
+# b=6
+#AIRTHMETIC OPERATION 
+# print(f"the sum of {a} and {b } : " ,a+b)
+# print(f"the subraction of {a} and {b} : {a-b}" )
+# print(f"the  multiplication of {a} and {b} : {a*b}")
+# print(f"the  division of {a} and {b} : {a/b}")
+# print(f"the  modulus of {a} and {b} : {a%b}")
+# print(f"the power of {a}  : {a**2}")
+# print(f"the  float division of {a} and {b} : {a//b}")
+
+
+#SWAPPING 
+# b=a
+# a=b
+# print(a)
+# print(b)
+
+##INPUT THREE NUMBER ND FIND GREATEST OF ALL THREE
+
+
+# num1 = int(input("enter the number "))
+# num2 = int(input("enter the second number "))
+# num3 = int(input("enter the third number "))
+
+# if(num1>num2 and num1>num3):
+#     print(f"{num1} is greatest among three")
+# elif(num2>num1 and num2>num3):
+#     print(f"{num2} is the  greatest among three")
+# elif(num3>num1 and num3>num2):
+#      print(f"{num3} is the  greatest among three")
+# else:
+#      print(f"{num1}, {num2}, {num3} all are equal")
+
+
+    
+# ##dice rolling game
+
+# # import random 
+# # dice1 = random.randrange(1,7)
+# # dice2 = random.randrange(1,7)
+
+# # print("first dice has " , dice1 )
+# # print("second has : " ,dice2)
+
+# print("welcome to the two dice rolling ")
+
+# import math
+# base = int(input("enter the base of the triangle "))
+# height = int(input("enter the height of the triangle "))
+# hypotenus = math.sqrt(base**2 + height**2)
+# print("the hypotenus of the triangle is : " , hypotenus)
+
+# for i in range(1,11,2): ## here the last 2 is the sequence in which we are printing 
+#     print(i)
+# for i in range (5,55 , 5):
+#     print(i)
+ 
+# #
