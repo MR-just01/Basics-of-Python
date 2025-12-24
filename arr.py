363c81950f9e97edc768d239f02c94ee3bf371eb
MR-just01 <rawatmaya2908@gmail.com>
2025-12-24 19:00:16 +0530

    initial commit

---
+++ arr.py
@@ -0,0 +1,11 @@
+arr = [2,4,1,6,3]
+print(arr[0])
+
+arr[0] = 10
+print(f"updated value of arr[0] : {arr[0]}")
+
+print(f" lenght of  : {len(arr)}")
+
+print("traversing the array : ")
+for i in  arr:
+    print(i)
